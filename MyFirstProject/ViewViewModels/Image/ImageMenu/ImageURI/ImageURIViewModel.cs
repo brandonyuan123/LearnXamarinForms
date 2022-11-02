79f99f97d0c7fe4cef7c507dcb05248e5f83522a
brandonyuan123 <brandonyuan05@gmail.om>
2022-11-02 09:55:19 -0500

    11/2

--- MyFirstProject/ViewViewModels/Image/ImageMenu/ImageURI/ImageURIViewModel.cs
+++ MyFirstProject/ViewViewModels/Image/ImageMenu/ImageURI/ImageURIViewModel.cs
@@ -10,6 +10,7 @@ namespace MyFirstProject.ViewViewModels.Image.ImageURI
     class ImageURIViewModel :BaseViewModel
     {
         public ImageSource ImageSrc { get; set; }
+
         public ImageURIViewModel()
         {
             Title = Titles.ImageURITitle;
@@ -21,7 +22,6 @@ namespace MyFirstProject.ViewViewModels.Image.ImageURI
             var imgsrc = new UriImageSource {Uri = new Uri(Images.ImageURI)};
             imgsrc.CachingEnabled = false;
             imgsrc.CacheValidity = TimeSpan.FromHours(1);
-
             return imgsrc;
         }
     }
