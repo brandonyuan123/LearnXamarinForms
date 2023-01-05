252d1d4f0fe56d6f2b8a5d74796d13084480585d
brandonyuan123 <brandonyuan05@gmail.om>
2023-01-05 11:11:11 -0600

    1/5 recent w errors

--- MyFirstProject/ViewViewModels/Controls/ControlsViewModel.cs
+++ MyFirstProject/ViewViewModels/Controls/ControlsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using MyFirstProject.Models;
 using MyFirstProject.ViewModels;
 using MyFirstProject.ViewViewModels.Controls.Slider;
+using MyFirstProject.ViewViewModels.Controls.Stepper;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,16 +13,22 @@ namespace MyFirstProject.ViewViewModels.Controls
     public class ControlsViewModel : BaseViewModel
     {
         public ICommand OnSliderClicked { get; set; }
+        public ICommand OnStepperClicked { get; set; }
 
         public ControlsViewModel()
         {
             Title = Titles.ControlsTitle;
             OnSliderClicked = new Command(OnSliderClickedAsync);
+            OnStepperClicked = new Command(OnStepperClickedAsync);
         }
 
         private async void OnSliderClickedAsync(object obj)
         {
             await Application.Current.MainPage.Navigation.PushAsync(new SliderView());
+        }
+        private async void OnStepperClickedAsync(object obj)
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new StepperView());
         }
     }
 }
---
+++ MyFirstProject/ViewViewModels/Controls/Stepper/StepperView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyFirstProject.ViewViewModels.Controls.Stepper
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StepperView : ContentPage
+    {
+        public StepperView()
+        {
+            InitializeComponent();
+            this.BindingContext = new ControlsViewModel();
+        }
+
+        public void OnStepperValueChanged (object sender, ValueChangedEventArgs e)
+        {
+            double value _e.NewValue;
+            _rotatingLabel Rotation = value;
+            _displayLabel.text = string.Format("The Stepper value is {0}", value);
+        }
+    }
+}
