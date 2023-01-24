3183f6ec8ac0a79fafb041ced38722c7757e852e
brandonyuan123 <brandonyuan05@gmail.om>
2023-01-24 10:27:05 -0600

    1/24

--- MyFirstProject/ViewViewModels/Controls/DualPicker/VMPicker/VMPickerViewModel.cs
+++ MyFirstProject/ViewViewModels/Controls/DualPicker/VMPicker/VMPickerViewModel.cs
@@ -10,12 +10,37 @@ namespace MyFirstProject.ViewViewModels.Controls.DualPicker.VMPicker
 {
     class VMPickerViewModel : BaseViewModel
     {
+        private string _selectedItem;
+
         public ImageSource SubmitImageSrc { get; set; }
 
         public VMPickerViewModel()
         {
             Title = Titles.PickerTitle;
             SubmitImageSrc = ImageSource.FromResource("MyFirstProject.Images.SubmitButton.png");
+        }
+
+        public string SelectedItem
+        {
+            get { return _selectedItem; }
+            set
+            {
+                if (_selectedItem != value)
+                {
+                    SetProperty(ref _selectedItem, value);
+                }
+            }
+        }
+
+        private async void OnSubmitClickedAsync(object obj)
+        {
+            if (string.IsNullOrEmpty(_selectedItem))
+            {
+                await Application.Current.MainPage.DisplayAlert(Titles.PickerTitle, "A selection must be made", "Ok");
+                return;
+            }
+
+            //stopped here last time
         }
     }
 }
